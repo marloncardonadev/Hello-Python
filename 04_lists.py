ce279878a3d3efb212cf3841685ba64e38ea3151
Marlon Cardona <marlon18_@hotmail.com>
2023-09-20 03:59:21 -0500

    Commit clase 04_lists.py

---
+++ 04_lists.py
@@ -0,0 +1,73 @@
+
+### Lists ###
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [35, 24, 62, 52, 30, 30, 17]
+
+print(my_list)
+print(len(my_list))
+
+my_other_list = [35, 1.88, "Marlon", "Cardona"]
+print(my_other_list)
+
+print(type(my_other_list))
+
+print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[-1])
+print(my_other_list[-4])
+print(my_list.count(30))
+# print(my_other_list[4]) IndexError
+# print(my_other_list[-5]) IndexError
+
+age, height, name, surname = my_other_list
+print(name)
+
+name, height, age, surname = my_other_list[2], my_other_list[1], my_other_list[0], my_other_list[3]
+print(surname)
+
+print(my_list + my_other_list)
+# print(my_list + my_other_list) Error
+
+my_other_list.append("MarlonDev")
+print(my_other_list)
+
+my_other_list.insert(1, "Rojo")
+print(my_other_list)
+
+my_other_list[1] = "Verde"
+print(my_other_list)
+
+my_other_list.remove("Verde")
+print(my_other_list)
+
+my_list.remove(30)
+print(my_list)
+
+print(my_list.pop())
+print(my_list)
+
+my_pop_element = my_list.pop(2)
+print(my_pop_element)
+print(my_list)
+
+del my_list[2]
+print(my_list)
+
+my_new_list = my_list.copy()
+
+my_list.clear()
+print(my_list)
+print(my_new_list)
+
+my_new_list.reverse()
+print(my_new_list)
+
+my_new_list.sort()
+print(my_new_list)
+
+print(my_new_list[1:3])
