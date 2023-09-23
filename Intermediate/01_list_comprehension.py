acc636d0d277b05bed9b8c29700d8ec14c8f82d7
Marlon Cardona <marlon18_@hotmail.com>
2023-09-23 00:39:09 -0500

    Commit curso intermediate 00 y 01

---
+++ Intermediate/01_list_comprehension.py
@@ -0,0 +1,29 @@
+
+### List Comprehension ###
+
+my_original_list = [0, 1, 2, 3, 4, 5, 6, 7]
+print(my_original_list)
+
+my_range = range(8)
+print(list(my_range))
+
+my_list = [i + 1 for i in range(8)]
+print(my_list)
+
+my_list = [i * 2 for i in range(8)]
+print(my_list)
+
+def sum_five(number):
+    return number + 5
+
+my_list = [sum_five(i) for i in range(8)]
+print(my_list)
+
+def fib(number):
+    if number < 2:
+        return number
+    else:
+        return fib(number - 1) + fib(number - 2)
+
+my_fib = [fib(i) for i in range(10)]
+print(my_fib)
