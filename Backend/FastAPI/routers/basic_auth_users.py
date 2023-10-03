b4aa92cc29dad89fe9a4aabba71602520e915949
Marlon Cardona <marlon18_@hotmail.com>
2023-10-03 03:20:07 -0500

    Commit clase oauth2 y JWT Python

---
+++ Backend/FastAPI/routers/basic_auth_users.py
@@ -0,0 +1,67 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+router = APIRouter()
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+
+
+class User(BaseModel):
+    username: str
+    full_name: str
+    email: str
+    dasabled: bool
+
+class UserDB(User):
+    password: str
+
+users_db = {
+    "marlondev": {
+        "username": "marlondev",
+        "full_name": "Marlon Cardona",
+        "email": "marloncardona@marlondev.com",
+        "dasabled": False,
+        "password": "123456"
+    },
+    "marlondev2": {
+        "username": "marlondev2",
+        "full_name": "Marlon Cardona 2",
+        "email": "marloncardon2@marlondev.com",
+        "dasabled": True,
+        "password": "abcdef"
+    }
+}
+
+def search_user_db(username: str):
+    if username in users_db:
+        return UserDB(**users_db[username])
+
+def search_user(username: str):
+    if username in users_db:
+        return User(**users_db[username])
+
+async def current_user(token: str = Depends(oauth2)):
+    user = search_user(token)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciales de autenticación inválidas", headers={"WWW-Authenticate": "Bearer"})
+    if user.dasabled:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Usuario inactivo")
+    return user
+
+
+@router.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    user_db = users_db.get(form.username)
+    if not users_db:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
+    user = search_user_db(form.username)
+    if not form.password == user.password:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@router.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
